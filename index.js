13537463d4b2badb865a87eed9ac74ed258d2220
BARON <167087373+xviibr@users.noreply.github.com>
2025-07-06 20:39:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
     .setName('𝐯𝐱𝐥𝐞𝐧.𝐳')
     
  .setAssetsLargeImage('https://media.discordapp.net/attachments/1373190165274558616/1389995739534327838/0e1f8367d81e6f161d198a0b5011a62f.gif?ex=6866a5e9&is=68655469&hm=1e3dc491274ac17536bb71bbb9d1cd493fdeec6e7f273cf11a954e71aae8c74e&=&width=500&height=385') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('𝟏𝟎𝟎𝟐 𝐃𝐀𝐘 𝐖𝐈𝐓𝐇 𝐔') //Text when you hover the Large image
+    .setAssetsLargeText('𝟏𝟎𝟎𝟔 𝐃𝐀𝐘 𝐖𝐈𝐓𝐇 𝐔') //Text when you hover the Large image
    
     .addButton('𝐦𝐞', 'https://linktr.ee/br0510')
   .addButton(`𝐒𝐭𝐞𝐚𝐦`, `https://steamcommunity.com/id/nopeeth/`);
